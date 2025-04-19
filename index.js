624dd454a2a89e0e781163d5918804b84db62200
César Augusto Saviñón <grulodeloteria@gmail.com>
2025-04-19 04:22:17 +0200

    Versión final del backend GRULODE funcionando

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/grulode', { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('Conectado a MongoDB'))
-.catch(err => console.error('No se pudo conectar a MongoDB', err));
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('GRULODE Backend funcionando'));
+app.get('/', (req, res) => {
+  res.send('¡GRULODE Backend está en línea!');
+});
 
-app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000'));
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
