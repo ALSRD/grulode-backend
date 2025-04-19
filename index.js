db0f8660f4d437d48d6619c381bc19f2895cc695
César Augusto Saviñón <grulodeloteria@gmail.com>
2025-04-19 20:36:49 +0200

    Versión creada manualmente del backend GRULODE

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡GRULODE Backend está en línea!');
+  res.send('GRULODE Backend está en línea!');
 });
 
 app.listen(PORT, () => {
